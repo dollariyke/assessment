dc33186b4a1fd2bf35d7337c340d77d4762f1554
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-30 16:28:54 +0400

    Update App.js

--- assessment2/src/App.js
+++ assessment2/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './App.css';
+import Books from './component/Books'
+import BookDetails from './component/BookDetails'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
         <h1>React Assessment</h1>
+        <Router>
+        <Route path='/books' exact component={Books}/>
+        <Route path='/book-details/:idThatWeExpect' component={BookDetails}/>
+        </Router>
+        
     </div>
   );
+
 }
 
 export default App;
